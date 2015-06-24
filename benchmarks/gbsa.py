bde8c0ffd15cdf3838981fcc2c45287f40cd5f5e
cxh <cxh@stanford.edu>
2015-06-24 00:39:24 -0700

    New results

--- benchmarks/gbsa.py
+++ benchmarks/gbsa.py
@@ -1,6 +1,6 @@
 from . import create_benchmark
 
-@create_benchmark(test='gbsa', platform='OpenCL', cutoff=0.9, heavy=False, device='0', precision='mixed')
+@create_benchmark(test='gbsa', platform='OpenCL', cutoff=0.9, heavy=False, device='2', precision='mixed')
 def track_opencl(): pass
 @create_benchmark(test='gbsa', platform='CUDA', cutoff=0.9, heavy=False, device='0', precision='mixed')
 def track_cuda(): pass
--- populate.sh
+++ populate.sh
@@ -1,10 +1,10 @@
-set -e -x
+set -e -x 
 
 echo "For a new machine, run asv on all  the commits that have been benchmarked on other machines"
 
 git checkout master
 git pull origin master
-asv run -k EXISTING
+asv run -k EXISTING --machine vsp-fah-nvidia
 git add -u results
 git add results
 git commit -m "New results" -a || true
